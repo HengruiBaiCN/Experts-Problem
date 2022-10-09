3900df14a13d23d37d5b12d9643d2e91e60660bf
Hengrui Bai <u6968198@anu.edu.au>
2022-10-09 17:12:37 +1100

    initialisation

---
+++ expert.py
@@ -0,0 +1,23 @@
+from ast import Pow
+from cmath import log
+import random
+from itertools import chain, combinations
+from re import A
+import time
+import matplotlib.pyplot as plt
+import math
+
+import numpy as np
+
+
+def A():
+    return None
+
+def B():
+    return None
+
+def C():
+    return None
+
+def D():
+    return None
---
+++ main.py
@@ -0,0 +1,26 @@
+from ast import Pow
+from cmath import log
+import random
+from itertools import chain, combinations
+from re import A
+import time
+import matplotlib.pyplot as plt
+import math
+import expert
+
+import numpy as np
+
+
+def weighted_majority():
+    """_summary_
+    
+    Returns:
+        _type_: _description_
+    """
+    expert.expert_A
+    
+    return None
+
+
+def multiplicative_weights_update():
+    return None
